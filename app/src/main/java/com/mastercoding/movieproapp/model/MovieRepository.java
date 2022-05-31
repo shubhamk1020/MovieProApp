d883425906f9320d07b24c6c7be5d8464095092f
shubham1020k <gyaninfo128@gmail.com>
2022-05-31 09:05:27 +0530

    MovieDataService MovieRepository RetrofitInstance strings.xml

--- app/src/main/java/com/mastercoding/movieproapp/model/MovieRepository.java
+++ app/src/main/java/com/mastercoding/movieproapp/model/MovieRepository.java
@@ -4,10 +4,16 @@ import android.app.Application;
 
 import androidx.lifecycle.MutableLiveData;
 
+import com.mastercoding.movieproapp.R;
 import com.mastercoding.movieproapp.service.MovieDataService;
+import com.mastercoding.movieproapp.service.RetrofitInstance;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MovieRepository {
 
@@ -20,6 +26,26 @@ public class MovieRepository {
     }
 
     public MutableLiveData<List<Movie>> getMutableLiveData(){
-        MovieDataService movieDataService = RetrofitInstance.
+        MovieDataService movieDataService = RetrofitInstance.getService();
+
+        Call<Result> call = movieDataService.getPopularMovies(application.getApplicationContext().getString(R.string.api_key));
+
+        call.enqueue(new Callback<Result>() {
+            @Override
+            public void onResponse(Call<Result> call, Response<Result> response) {
+                Result result = response.body();
+
+                if(result != null && result.getResults() != null){
+                    movies = (ArrayList<Movie>) result.getResults();
+                    mutableLiveData.setValue(movies);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Result> call, Throwable t) {
+
+            }
+        });
+        return mutableLiveData;
     }
 }
